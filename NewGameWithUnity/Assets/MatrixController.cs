da2aa374b2120d3eb5c0f59ecb6a99b339e6c3b7
EmirGaziKopar <75802906+EmirGaziKopar@users.noreply.github.com>
2021-07-11 14:59:33 +0300

    1.8

---
+++ NewGameWithUnity/Assets/MatrixController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatrixController : MonoBehaviour
+{
+    [SerializeField] new ParticleSystem particleSystem;
+
+    public float baslangic=0f;
+
+    public float bitis = 50f;
+    private void Start()
+    {
+
+        particleSystem = GetComponent<ParticleSystem>();
+        var emission = particleSystem.emission;
+        emission.rateOverTime = baslangic;
+
+
+    }
+
+
+    private void Update()
+    {
+        if (Bocek.BocekisDead == true)
+        {
+            var emission = particleSystem.emission;
+            emission.rateOverTime = bitis;
+        }
+        
+    }
+
+
+
+}
